49e6777cb1b8f413faf610e81d41f02b86d7dcdd
sdk-quadra <sadaka556@gmail.com>
2020-02-07 13:24:14 +0900

    routes修正

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ before do
   @title = "メモアプリ"
   @ext = ".txt"
   @memo_loc = "./public/memo/"
+  @prefix = "メモ"
+  @delimiter = "_"
   @disp_add = "off"
 end
 
@@ -16,8 +18,8 @@ helpers do
     dir_files = dir_files.select { |f| f unless f[0].include?(".") }  # 不可視ファイル除外
   end
 
-  def f_content(file_num)
-    file = File.open(@memo_loc + file_num)
+  def f_content(id)
+    file = File.open(@memo_loc + id + @ext)
     content = file.read
   end
 end
@@ -25,16 +27,19 @@ end
 get "/" do
   @disp_add = "on"
   dir_files = dir
-  @is_empty = "まだメモはありません" if dir_files.empty?
+  @is_empty = "まだメモがありません.追加してください" if dir_files.empty?
 
-  files = Hash.new
+  files = {}
   dir_files.map { |b|
-    b.gsub!(/#{@ext}/, "")
-    zenkaku_num = b.tr("0-9", "０-９")  # 表示のファイル名数字は全角
-    disp_fname = "メモ" + zenkaku_num.to_s
-    files[b] = disp_fname
+    key = b.gsub(/#{@delimiter}(.*)/, "")
+    fname = b.gsub!(/#{@ext}/, "")
+
+    zenkaku_num = key.tr("0-9", "０-９")  # 表示のファイル名数字は全角
+    disp_fname = @prefix + zenkaku_num.to_s
+    files[fname] = disp_fname
   }
-  @files = files.sort_by { |k, v| k.to_i }.reverse.to_h  # keyでsort
+
+  @files = files.sort.reverse.to_h  # keyでsort
 
   erb :index
 end
@@ -43,48 +48,50 @@ get "/new" do
   erb :new
 end
 
-post "/new" do
+post "/" do
   memo = params[:memo]
-
-  trim_memo = memo.gsub(/[\p{Z}\t\r\n\v\f]/, "")
-  if trim_memo.size < 1  # 空文字は保存しない
-    redirect "/new"
-  end
-
   dir_files = dir
-  file_num = dir_files.map! { |b| b.gsub(/#{@ext}/, "").to_i }
-  last_file_num = file_num.max + 1
 
-  File.open(@memo_loc + last_file_num.to_s + @ext, "w") do |f|
+  unless dir_files.empty?
+    file_num = dir_files.map! { |b| b.gsub(/#{@delimiter}(.*)/, "").to_i }
+    last_file_num = file_num.max + 1
+  else
+    last_file_num = 1
+  end
+  
+  new_file_name = last_file_num.to_s + @delimiter + SecureRandom.uuid
+
+  File.open(@memo_loc + new_file_name + @ext, "w") do |f|
     f.write(memo)
   end
   redirect "/"
 end
 
-get "/:file_num" do
-  file_num = params[:file_num]
-  @content = f_content(file_num + @ext)
+get "/:id" do
+  id = params[:id]
+  @content = f_content(id)
   @content.gsub!(/\n/, "<br>")
   erb :show
 end
 
-get "/:file_num/edit" do
-  file_num = params[:file_num]
-  @content = f_content(file_num + @ext)
+get "/:id/edit" do
+  id = params[:id]
+  @content = f_content(id)
   erb :edit
 end
 
-patch "/:file_num" do
-  file_num = params[:file_num]
+patch "/:id" do
+  id = params[:id]
   memo = params[:memo]
-  File.open(@memo_loc + file_num + @ext, "w") do |f|
+  File.open(@memo_loc + id + @ext, "w") do |f|
+    f.flock(File::LOCK_EX)
     f.write(memo)
   end
   redirect "/"
 end
 
-delete "/:file_num" do
-  file_num = params[:file_num]
-  File.delete(@memo_loc + file_num + @ext)
+delete "/:id" do
+  id = params[:id]
+  File.delete(@memo_loc + id + @ext)
   redirect "/"
 end
