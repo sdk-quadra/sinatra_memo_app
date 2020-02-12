b90ef6c61cec6b6e9b504a48aeae90c7c1a53737
sdk-quadra <sadaka556@gmail.com>
2020-02-12 19:50:43 +0900

    保存先をDBに変更

--- app.rb
+++ app.rb
@@ -2,42 +2,40 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "pg"
 
 before do
   @title = "メモアプリ"
-  @ext = ".txt"
-  @memo_loc = "./public/memo/"
   @prefix = "メモ"
-  @delimiter = "_"
   @disp_add = "off"
+
+  @db_conn = PG.connect(dbname: "app")
+  @table = "memos"
+  @col_content = "content"
 end
 
 helpers do
-  def dir
-    dir_files = Dir.entries(@memo_loc)
-    dir_files = dir_files.select { |f| f unless f[0].include?(".") }  # 不可視ファイル除外
-  end
-
-  def f_content(id)
-    file = File.open(@memo_loc + id + @ext)
-    content = file.read
+  def transact(sql)
+    @db_conn.exec("BEGIN")
+    if @db_conn.exec(sql)
+      @db_conn.exec("COMMIT")
+    else
+      @db_conn.exec("ROLLBACK")
+    end
   end
 end
 
 get "/" do
   @disp_add = "on"
-  dir_files = dir
-  @is_empty = "まだメモがありません.追加してください" if dir_files.empty?
-
   files = {}
-  dir_files.map { |b|
-    key = b.gsub(/#{@delimiter}(.*)/, "")
-    fname = b.gsub!(/#{@ext}/, "")
 
-    zenkaku_num = key.tr("0-9", "０-９")  # 表示のファイル名数字は全角
-    disp_fname = @prefix + zenkaku_num.to_s
-    files[fname] = disp_fname
-  }
+  memo_records = @db_conn.exec("SELECT * FROM #{@table}")
+  @is_empty = "メモがありません.追加してください" if memo_records.count < 1
+
+  memo_records.each do |record|
+    zenkaku_id = record["id"].tr("0-9", "０-９")  # 表示は全角
+    files[record["id"]] = @prefix + zenkaku_id
+  end
 
   @files = files.sort.reverse.to_h  # keyでsort
 
@@ -50,48 +48,41 @@ end
 
 post "/" do
   memo = params[:memo]
-  dir_files = dir
-
-  unless dir_files.empty?
-    file_num = dir_files.map! { |b| b.gsub(/#{@delimiter}(.*)/, "").to_i }
-    last_file_num = file_num.max + 1
-  else
-    last_file_num = 1
-  end
-  
-  new_file_name = last_file_num.to_s + @delimiter + SecureRandom.uuid
-
-  File.open(@memo_loc + new_file_name + @ext, "w") do |f|
-    f.write(memo)
-  end
+  sql = "INSERT INTO #{@table} (#{@col_content}) values (E'#{memo}')"
+  transact(sql)
   redirect "/"
 end
 
 get "/:id" do
   id = params[:id]
-  @content = f_content(id)
+  content = @db_conn.exec("SELECT #{@col_content} FROM #{@table} WHERE id = #{id}")
+  content.each do |c|
+    @content = c["#{@col_content}"]
+  end
   @content.gsub!(/\n/, "<br>")
   erb :show
 end
 
 get "/:id/edit" do
   id = params[:id]
-  @content = f_content(id)
+  content = @db_conn.exec("SELECT #{@col_content} FROM #{@table} WHERE id = #{id}")
+  content.each do |c|
+    @content = c["#{@col_content}"]
+  end
   erb :edit
 end
 
 patch "/:id" do
   id = params[:id]
   memo = params[:memo]
-  File.open(@memo_loc + id + @ext, "w") do |f|
-    f.flock(File::LOCK_EX)
-    f.write(memo)
-  end
+  sql = "UPDATE #{@table} SET #{@col_content} = E'#{memo}' WHERE id = #{id}"
+  transact(sql)
   redirect "/"
 end
 
 delete "/:id" do
   id = params[:id]
-  File.delete(@memo_loc + id + @ext)
+  sql = "DELETE FROM #{@table} where id = #{id}"
+  transact(sql)
   redirect "/"
 end
